306b1519ed974a7c19ae04dcb58f34e464c50fc3
Artur Michalek <52135326+cptartur@users.noreply.github.com>
2021-11-04 18:56:43 +0100

    Store initial commit

---
+++ store/app/src/main/java/com/example/store/Register.kt
@@ -0,0 +1,12 @@
+package com.example.store
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_register)
+    }
+}
