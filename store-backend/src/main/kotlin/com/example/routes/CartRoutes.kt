bca514a3e2442de680be249480c01e531f6b9f48
Artur Michalek <52135326+cptartur@users.noreply.github.com>
2021-12-07 16:01:39 +0100

    Rename repository variables, add requests responses

--- store-backend/src/main/kotlin/com/example/routes/CartRoutes.kt
+++ store-backend/src/main/kotlin/com/example/routes/CartRoutes.kt
@@ -6,14 +6,15 @@ import com.example.repositories.ProductRepository
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Application.configureCartRoutes() {
     val baseString = "cart"
-    val repository: CartRepository = CartRepository
-    val carts: CartProductRepository = CartProductRepository
+    val cartRepository = CartRepository
+    val cartProductRepository = CartProductRepository
     routing {
         get("/$baseString/{user_id}") {
             val id = call.parameters["user_id"]?.toInt()
@@ -24,13 +25,17 @@ fun Application.configureCartRoutes() {
 
         get("/$baseString/{user_id}/{cart_id}") {
             val id = call.parameters["cart_id"]?.toInt()
-            id?.let { it -> carts.findById(it) }?.let { it -> call.respond(it) }
+            id?.let { it -> cartProductRepository.findById(it) }?.let { it -> call.respond(it) }
         }
 
         post("/$baseString/{user_id}") {
             val userID = call.parameters["user_id"]?.toInt()
             if (userID != null) {
-                repository.create(userId = userID)
+                val id = cartRepository.create(userId = userID)
+                call.response.apply {
+                    status(HttpStatusCode.Created)
+                    headers.append(HttpHeaders.Location, id.toString())
+                }
             }
         }
 
@@ -42,6 +47,7 @@ fun Application.configureCartRoutes() {
                 val product = ProductRepository.findById(it.id)
                 if (product != null && cartID != null) {
                     CartProductRepository.createOrUpdate(cartID, product, it.amount)
+                    call.response.status(HttpStatusCode.OK)
                 }
             }
         }
@@ -49,8 +55,8 @@ fun Application.configureCartRoutes() {
         delete("/$baseString/{cart_id}") {
             val cartID = call.parameters["cart_id"]?.toInt()
             if (cartID != null) {
-                repository.remove(cartID)
-                carts.removeAll(cartID)
+                cartRepository.remove(cartID)
+                cartProductRepository.removeAll(cartID)
             }
         }
 
@@ -58,11 +64,13 @@ fun Application.configureCartRoutes() {
             val cartID = call.parameters["cart_id"]?.toInt()
             val productID = call.parameters["product_id"]?.toInt()
             if (cartID != null && productID != null) {
-                carts.remove(cartID, productID)
+                if (cartProductRepository.remove(cartID, productID)) {
+                    call.response.status(HttpStatusCode.OK)
+                }
             }
         }
     }
 }
 
-data class ProductAndAmount(val id: Int, val amount: Int) {}
-inline fun <reified T> Gson.fromJson(json: String) = fromJson<T>(json, object: TypeToken<T>() {}.type)
+private data class ProductAndAmount(val id: Int, val amount: Int) {}
+inline fun <reified T> Gson.fromJson(json: String): T = fromJson(json, object: TypeToken<T>() {}.type)
