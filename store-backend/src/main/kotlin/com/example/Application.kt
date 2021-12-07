17ed3291697fe327e07da5929bffd48f139b70ae
Artur Michalek <52135326+cptartur@users.noreply.github.com>
2021-12-07 16:01:54 +0100

    Cleanup application launch function

--- store-backend/src/main/kotlin/com/example/Application.kt
+++ store-backend/src/main/kotlin/com/example/Application.kt
@@ -8,10 +8,7 @@ import com.example.repositories.ProductRepository
 import com.example.repositories.UserRepository
 import com.example.routes.configureCRUDRoutes
 import com.example.routes.configureCartRoutes
-import com.example.tables.Carts
-import com.example.tables.CartsProducts
-import com.example.tables.Products
-import com.example.tables.Users
+import com.example.tables.*
 import io.ktor.application.*
 import io.ktor.features.*
 import org.jetbrains.exposed.sql.*
@@ -24,26 +21,20 @@ fun main() {
 //    Database.connect("jdbc:sqlite:file:test?mode=memory&cache=shared", "org.sqlite.JDBC")
     transaction {
         addLogger(StdOutSqlLogger)
-
         SchemaUtils.create(Users)
         SchemaUtils.create(Products)
         SchemaUtils.create(Carts)
         SchemaUtils.create(CartsProducts)
-
-//        Users.insert {
-//            it[name] = "Artur"
-//        }
+        SchemaUtils.create(Addresses)
     }
     TransactionManager.manager.defaultIsolationLevel =
         Connection.TRANSACTION_SERIALIZABLE
     embeddedServer(Netty, port = 8080, host = "localhost") {
-//        configureUserRoutes()
         configureSerialization()
         configureCRUDRoutes("user", UserRepository)
         configureCRUDRoutes("product", ProductRepository)
         configureCRUDRoutes("category", CategoryRepository)
         configureCartRoutes()
-//        configureProductRoutes()
         configureRouting()
         install(StatusPages) {
 
