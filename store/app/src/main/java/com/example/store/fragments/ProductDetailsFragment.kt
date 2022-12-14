8ca44fe252537d661569cd8dda221cdf46f2d407
Artur Michalek <52135326+cptartur@users.noreply.github.com>
2022-12-14 22:32:26 +0100

    Add missing files

---
+++ store/app/src/main/java/com/example/store/fragments/ProductDetailsFragment.kt
@@ -0,0 +1,45 @@
+package com.example.store.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.example.store.R
+import com.example.store.databinding.FragmentLocalLoginBinding
+import com.example.store.databinding.FragmentProductDetailsBinding
+import com.example.store.repositories.ProductRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+
+
+class ProductDetailsFragment : Fragment() {
+    private val args: ProductDetailsFragmentArgs by navArgs()
+    private var productId: Int? = null
+
+    private lateinit var binding: FragmentProductDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProductDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        productId = args.productId
+        Log.d("Details", "Product id is $productId")
+
+        val product = productId?.let {
+            runBlocking(Dispatchers.IO) {
+                ProductRepository.getProduct(it)
+            }
+        }
+        binding.productDetailsText.text = product?.description ?: "Could not fetch product."
+    }
+}
